06fb3d9af30c6d3a6ff107c6105565b19406d1d0
Christian Schlosrich <christian.schlosrich@make.com.au>
2013-07-17 16:10:26 +1000

    upd

---
+++ wonders-of-vectors/build/compile.sh
@@ -0,0 +1,9 @@
+java -jar compiler.jar \
+--js ../js/RigidBody.js \
+--js ../js/Ball.js \
+--js ../js/Floor.js \
+--js ../js/CanvasRenderer.js \
+--js ../js/Vector.js \
+--js ../js/World.js \
+--js ../js/Simulation1.js \
+--js_output_file ../js/Simulation1.min.js
